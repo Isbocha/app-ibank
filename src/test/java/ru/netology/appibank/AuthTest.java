c76e40e6e39b51e25304fc436b3bd3a43aa64441
InnaB <isbocha@gmail.com>
2023-08-21 17:56:51 +0300

    new edition

--- src/test/java/ru/netology/appibank/AuthTest.java
+++ src/test/java/ru/netology/appibank/AuthTest.java
@@ -26,9 +26,6 @@ class AuthTest {
     @DisplayName("Should successfully login with active registered user")
     void shouldSuccessfulLoginIfRegisteredActiveUser() {
         var registeredUser = getRegisteredUser("active");
-        // TODO: добавить логику теста, в рамках которого будет выполнена попытка входа в личный кабинет с учётными
-        //  данными зарегистрированного активного пользователя, для заполнения полей формы используйте
-        //  пользователя registeredUser
         $("[data-test-id=login] .input__control").setValue(registeredUser.getLogin());
         $("[data-test-id=password] .input__control").setValue(registeredUser.getPassword());
         $("[data-test-id=action-login]").click();
@@ -40,8 +37,6 @@ class AuthTest {
     @DisplayName("Should get error message if login with not registered user")
     void shouldGetErrorIfNotRegisteredUser() {
         var notRegisteredUser = getUser("active");
-        // TODO: добавить логику теста в рамках которого будет выполнена попытка входа в личный кабинет
-        //  незарегистрированного пользователя, для заполнения полей формы используйте пользователя notRegisteredUser
         $("[data-test-id=login] .input__control").setValue(notRegisteredUser.getLogin());
         $("[data-test-id=password] .input__control").setValue(notRegisteredUser.getPassword());
         $("[data-test-id=action-login]").click();
@@ -53,8 +48,6 @@ class AuthTest {
     @DisplayName("Should get error message if login with blocked registered user")
     void shouldGetErrorIfBlockedUser() {
         var blockedUser = getRegisteredUser("blocked");
-        // TODO: добавить логику теста в рамках которого будет выполнена попытка входа в личный кабинет,
-        //  заблокированного пользователя, для заполнения полей формы используйте пользователя blockedUser
         $("[data-test-id=login] .input__control").setValue(blockedUser.getLogin());
         $("[data-test-id=password] .input__control").setValue(blockedUser.getPassword());
         $("[data-test-id=action-login]").click();
@@ -67,9 +60,6 @@ class AuthTest {
     void shouldGetErrorIfWrongLogin() {
         var registeredUser = getRegisteredUser("active");
         var wrongLogin = getRandomLogin();
-        // TODO: добавить логику теста в рамках которого будет выполнена попытка входа в личный кабинет с неверным
-        //  логином, для заполнения поля формы "Логин" используйте переменную wrongLogin,
-        //  "Пароль" - пользователя registeredUser
         $("[data-test-id=login] .input__control").setValue(wrongLogin);
         $("[data-test-id=password] .input__control").setValue(registeredUser.getPassword());
         $("[data-test-id=action-login]").click();
@@ -82,9 +72,6 @@ class AuthTest {
     void shouldGetErrorIfWrongPassword() {
         var registeredUser = getRegisteredUser("active");
         var wrongPassword = getRandomPassword();
-        // TODO: добавить логику теста в рамках которого будет выполнена попытка входа в личный кабинет с неверным
-        //  паролем, для заполнения поля формы "Логин" используйте пользователя registeredUser,
-        //  "Пароль" - переменную wrongPassword
         $("[data-test-id=login] .input__control").setValue(registeredUser.getLogin());
         $("[data-test-id=password] .input__control").setValue(wrongPassword);
         $("[data-test-id=action-login]").click();
